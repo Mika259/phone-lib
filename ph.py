9e80cd127a39602e5fcc9aa6582680a4d0824d9e
M1K259 <87774553+Mika259@users.noreply.github.com>
2023-04-30 05:15:25 +0800

    Update ph.py

--- ph.py
+++ ph.py
@@ -1,7 +1,47 @@
+#import including libraries
+
+#this is important!!!!!!!!!!!!!!!!! 😂
 import phonenumbers
+
+import os
+import time as t
+#:v
 from  phonenumbers import geocoder, carrier, timezone
-number = input("Input Phonenumber : ")
-phone_number = phonenumbers.parse(number)
-print(f"Location : {geocoder.description_for_number(phone_number, 'en')}")
-print(f"Carrier : {carrier.name_for_number(phone_number, 'en')}")
-print(f"Timezone : {timezone.time_zones_for_number(phone_number)}")
+
+#color terminal
+b = '\033[34m'
+B = '\033[30m'
+R = '\033[31m'
+G = '\033[32m'
+C = '\033[36m'
+W = '\033[0m'
+Y = '\033[33m'
+
+#link :v
+urlGithub = 'https://github.com/Mika259'
+os.system('clear')
+print(f"\n      {R}\033[46m[×]{B}{urlGithub}{R}[×]\033[49m\n    ")
+
+#function :)
+def menu():
+        number = input(f"{G}Insert Phonenumber : {W}")
+        t.sleep(0.1)
+        phone_number = phonenumbers.parse(number)
+        print(f"[+]\n ╰➤ {Y}Location :{W} {geocoder.description_for_number(phone_number, 'en')}")
+        print(f" ╰➤ {Y}Carrier :{W} {carrier.name_for_number(phone_number, 'en')}")
+        print(f" ╰➤ {Y}Timezone :{W} {timezone.time_zones_for_number(phone_number)}")
+        t.sleep(0.5)
+        user = input(f"{C}\nmore (y/n) {W}")
+        if user == 'y':
+                menu()
+        elif user == 'n':
+                print(f"{b}Thanks For Using Me{W} :)")
+                exit()
+        else:
+                print(f"{R}Error Command!{W}")
+                exit()
+menu()
+#Tool by Mika259 😜
+#don't steal my tools without my permision (credits me)
+
+#these my water hand lah
